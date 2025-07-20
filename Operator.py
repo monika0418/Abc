6164bd7bc2b76ba71d3bf527b272ba3b7ba42838
Monika <monikamoni182006@gmail.com>
2025-07-20 16:19:15 +0530

    Create Operator.py

---
+++ Operator.py
@@ -0,0 +1,7 @@
+num1 = 5
+num2 = 10
+total = num1 + num2
+print(total)
+print(5 == 10) #comparison operation
+print(5 < 10 and 5!=10)#logical operation
+print(5+10 and 5-10)#addition & subtraction operation 
