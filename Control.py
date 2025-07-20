9c9bcaccec598f1cfad71dbe163feaa0857badf3
Monika <monikamoni182006@gmail.com>
2025-07-20 12:28:44 +0530

    Create Control.py

---
+++ Control.py
@@ -0,0 +1,13 @@
+mark =int(input("enter mark: "))
+if mark>100:
+    print("invalid mark")
+elif mark >=80:
+    print("pass")
+elif mark >=60:
+    print("pass")
+elif mark >=85:
+    print("pass")
+elif mark >=65:
+    print("pass")
+else:
+    print("Fail")
